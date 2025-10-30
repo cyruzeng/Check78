0e57f4c535e161dc5ccf1a83c1e213cab9fe314b
CyrusZeng <cyrus@cyruzeng.org>
2025-10-31 03:25:08 +0800

    update

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-import { MeasurementExperience } from "@/components/MeasurementExperience";
-
-export default function HomePage() {
-  return (
-    <main className="flex min-h-screen flex-col">
-      <MeasurementExperience />
-    </main>
-  );
-}
